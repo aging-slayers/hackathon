de2f66cdcb921ed5e8715351543d5be5aa1ae812
NetBUG <darikcr@gmail.com>
2025-07-24 11:47:46 +0400

    Fix escaping

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mkdir -p access
-echo Don't forget to put Google Sheets credentials to /access folder!
+echo Don\'t forget to put Google Sheets credentials to /access folder!
 wget https://nb3.me/public/aging-slayers-data.tar.gz
 tar -xf aging-slayers-data.tar.gz
 docker rm -f aging_slay_streamlit
