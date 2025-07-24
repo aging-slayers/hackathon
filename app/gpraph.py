a792d8e085da432c9546e5eebff770359eca4c76
NetBUG <darikcr@gmail.com>
2025-07-24 10:42:23 +0400

    New loading mechanism -- global

--- app/gpraph.py
+++ app/gpraph.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import project_config
 import json
+import time
 
 from research_scripts.find_id_compound import find_id_compound
 from research_scripts.get_actual_relations import get_actual_relations
@@ -11,10 +12,13 @@ from research_scripts.visualisation import plot_subgraph
 from research_scripts.save_graph import save_igraph_as_json
 from research_scripts.coloring_edges import coloring_edges
 
-
 from loguru import logger
 
-
+start = time.time()
+triplets = pd.read_csv(project_config.PATH_DRKG, sep='\t', header=None).values.tolist()
+# graph = ig.read(project_config.PATH_GRAPH, format="gml")
+logger.info(f"Loaded graph in {time.time() - start}s...")
+  
 
 def get_id_to_name_mapping():
     """Load the entity name mapping from a JSON file."""
@@ -28,19 +32,14 @@ def get_id_to_name_mapping():
     return entity_name_mapping
 
 def run_subgraph_builder(drugs):
-
-    triplets = pd.read_csv(project_config.PATH_DRKG, sep='\t', header=None).values.tolist()
-    
+    global triplets
     drug_ids = find_id_compound(drugs)
     
     logger.debug(f"Creating graph for the drugs: {', '.join(drugs)} with IDs: {', '.join(drug_ids)}")
-    
     actual_relations = get_actual_relations(drug_ids)
-
     actual_triplets = [triplet for triplet in triplets if triplet[1] in actual_relations]
     
     graph = build_igraph_from_triplets(actual_triplets)
-
     filt_graph = filter_graph(graph, drug_ids)
 
     filt_graph.vs['name'] = [get_id_to_name_mapping().get(name, name) for name in filt_graph.vs['name']]
