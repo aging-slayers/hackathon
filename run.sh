4be0426e8156b8194e206d3b01533b6979ffcd7f
NetBUG <darikcr@gmail.com>
2025-07-24 11:44:51 +0400

    Add running script

---
+++ run.sh
@@ -0,0 +1,9 @@
+mkdir -p access
+echo Don't forget to put Google Sheets credentials to /access folder!
+wget https://nb3.me/public/aging-slayers-data.tar.gz
+tar -xf aging-slayers-data.tar.gz
+docker rm -f aging_slay_streamlit
+docker image prune -a
+docker compose up -d
+docker exec --user root -it aging_slay_streamlit chown -R appuser access
+docker logs aging_slay_streamlit  -f
